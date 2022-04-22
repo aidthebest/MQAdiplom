ac8ff98e8b78274cd44a052bcb1199a27d31d640
Alexander Mizyukin <a.mizyukin@mail.ru>
2022-04-22 13:01:52 +0300

    test added 22 04 22

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/ClearTests.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/ClearTests.java
@@ -10,13 +10,16 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 //import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isNotEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
@@ -25,18 +28,28 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
+import static kotlinx.coroutines.flow.FlowKt.withIndex;
+
 import android.os.SystemClock;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.FrameLayout;
 
+import androidx.core.widget.NestedScrollView;
+import androidx.test.espresso.PerformException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.espresso.util.HumanReadables;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
 import org.junit.After;
@@ -46,6 +59,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.security.PublicKey;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import ru.iteco.fmhandroid.R;
 
@@ -55,8 +72,7 @@ import ru.iteco.fmhandroid.R;
 public class ClearTests {
 
 
-
-    ViewInteraction  maiMenuImageButton = onView(
+    ViewInteraction maiMenuImageButton = onView(
             allOf(withId(R.id.main_menu_image_button)));
 
     ViewInteraction claimsButton = onView(
@@ -76,7 +92,6 @@ public class ClearTests {
     ViewInteraction claimsFiltrerButton = onView((withId(R.id.filters_material_button)));
 
     ViewInteraction createClaimButton = onView(withId(R.id.add_new_claim_material_button));
-
 
     @Rule
     public ActivityTestRule<AppActivity> mActivityTestRule = new ActivityTestRule<>(AppActivity.class);
@@ -98,6 +113,85 @@ public class ClearTests {
                         && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
+    }
+
+    public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
+        return new TypeSafeMatcher<View>() {
+            int currentIndex = 0;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with index: ");
+                description.appendValue(index);
+                matcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                return matcher.matches(view) && currentIndex++ == index;
+            }
+        };
+    }
+
+    public static ViewAction scrollTo() {
+        return new ViewAction() {
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return Matchers.allOf(
+                        isDescendantOfA(isAssignableFrom(NestedScrollView.class)),
+                        withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE));
+            }
+
+            @Override
+            public String getDescription() {
+                return "View is not NestedScrollView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                try {
+                    NestedScrollView nestedScrollView = (NestedScrollView)
+                            findFirstParentLayoutOfClass(view);
+                    if (nestedScrollView != null) {
+                        nestedScrollView.scrollTo(0, view.getTop());
+                    } else {
+                        throw new Exception("Unable to find NestedScrollView parent.");
+                    }
+                } catch (Exception e) {
+                    throw new PerformException.Builder()
+                            .withActionDescription(this.getDescription())
+                            .withViewDescription(HumanReadables.describe(view))
+                            .withCause(e)
+                            .build();
+                }
+                uiController.loopMainThreadUntilIdle();
+            }
+
+        };
+    }
+    private static View findFirstParentLayoutOfClass(View view) {
+        ViewParent parent = new FrameLayout(view.getContext());
+        ViewParent incrementView = null;
+        int i = 0;
+        while (parent != null && !(parent.getClass() == NestedScrollView.class)) {
+            if (i == 0) {
+                parent = findParent(view);
+            } else {
+                parent = findParent(incrementView);
+            }
+            incrementView = parent;
+            i++;
+        }
+        return (View) parent;
+    }
+
+    private static ViewParent findParent(View view) {
+        return view.getParent();
+    }
+
+    private static ViewParent findParent(ViewParent view) {
+        return view.getParent();
     }
 
     @Before
@@ -134,19 +228,31 @@ public class ClearTests {
         materialTextView.perform(click());
     }
 
-    @Test
-    public void launchTest() {
+    public String getCurrentDate() {
+        Date currentDate = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        return dateFormat.format(currentDate);
+    }
 
+    public void goToClaimsScreen() {
+        SystemClock.sleep(2000);
+        maiMenuImageButton.perform(click());
+        SystemClock.sleep(2000);
+        claimsButton.perform(click());
+        SystemClock.sleep(2000);
     }
 
     @Test
-    public void goToClaimsFromMainButtonClick () {
+    public void launchTest() {
+    }
+
+    @Test
+    public void goToClaimsFromMainButtonClick() {
         SystemClock.sleep(1000);
         maiMenuImageButton.perform(click());
         claimsButton.perform(click());
         onView(withId(R.id.add_new_claim_material_button)).check(matches(isEnabled()));
     }
-
 
 
     @Test
@@ -168,12 +274,8 @@ public class ClearTests {
     }
 
     @Test
-    public void claimsFilteringConditionsCheck () {
-        SystemClock.sleep(2000);
-        maiMenuImageButton.perform(click());
-        SystemClock.sleep(2000);
-        claimsButton.perform(click());
-        SystemClock.sleep(2000);
+    public void claimsFilteringConditionsCheck() {
+        this.goToClaimsScreen();
         claimsFiltrerButton.perform(click());
         SystemClock.sleep(2000);
         ViewInteraction isOpen = onView((withId(R.id.item_filter_open))).check(matches(isChecked()));
@@ -187,12 +289,8 @@ public class ClearTests {
     }
 
     @Test
-    public void createClaimTest () {
-        SystemClock.sleep(2000);
-        maiMenuImageButton.perform(click());
-        SystemClock.sleep(2000);
-        claimsButton.perform(click());
-        SystemClock.sleep(2000);
+    public void createClaimTest() {
+        this.goToClaimsScreen();
         createClaimButton.perform(click());
         SystemClock.sleep(2000);
         ViewInteraction claimTitle = onView(withId(R.id.title_edit_text)).perform(replaceText("nice Title"));
@@ -200,12 +298,84 @@ public class ClearTests {
         SystemClock.sleep(2000);
         onView(withId(R.id.executor_drop_menu_auto_complete_text_view)).perform(replaceText("Лебедев Данил Александрович"));
         onView(withId(R.id.executor_drop_menu_auto_complete_text_view)).perform(click());
-        onView(withId(R.id.date_in_plan_text_input_edit_text)).perform(replaceText("25.04.2022"));
+        onView(withId(R.id.date_in_plan_text_input_edit_text)).perform(replaceText("25.04.2023"));
         onView(withId(R.id.time_in_plan_text_input_edit_text)).perform(replaceText("00:00"));
         onView(withId(R.id.description_edit_text)).perform(replaceText("nice sub"));
         onView(withId(R.id.description_edit_text)).perform(closeSoftKeyboard());
         onView(withId(R.id.save_button)).perform(click());
         SystemClock.sleep(2000);
         ViewInteraction claimsMainWindowCheck = onView(withText("Claims")).check(matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void createClaimWithEmptyFieldsTest() {
+        this.goToClaimsScreen();
+        createClaimButton.perform(click());
+        SystemClock.sleep(2000);
+        onView(withId(R.id.save_button)).perform(click());
+        SystemClock.sleep(2000);
+        onView(withText("Fill empty fields")).check(matches(isDisplayed()));
+        SystemClock.sleep(2000);
+        onView(withText("OK")).perform(click());
+        onView(withText("Cancel")).perform(click());
+        onView(withText("OK")).perform(click());
+    }
+
+    @Test
+    public void createClaimWithEmptyExecutorField() {
+        this.goToClaimsScreen();
+        createClaimButton.perform(click());
+        ViewInteraction claimTitle = onView(withId(R.id.title_edit_text)).perform(replaceText("nice Title"));
+        onView(withId(R.id.date_in_plan_text_input_edit_text)).perform(replaceText("25.04.2023"));
+        onView(withId(R.id.time_in_plan_text_input_edit_text)).perform(replaceText("00:00"));
+        onView(withId(R.id.description_edit_text)).perform(replaceText("nice sub"));
+        onView(withId(R.id.description_edit_text)).perform(closeSoftKeyboard());
+        onView(withId(R.id.save_button)).perform(click());
+        SystemClock.sleep(2000);
+        ViewInteraction claimsMainWindowCheck = onView(withText("Claims")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void createClaimTitleAndDescrConTest() {
+        this.goToClaimsScreen();
+        createClaimButton.perform(click());
+        SystemClock.sleep(2000);
+        ViewInteraction claimTitle = onView(withId(R.id.title_edit_text)).perform(replaceText("1>:{+ Тест Title "));
+        onView(withId(R.id.executor_drop_menu_auto_complete_text_view)).perform(click());
+        SystemClock.sleep(2000);
+        onView(withId(R.id.executor_drop_menu_auto_complete_text_view)).perform(replaceText("Лебедев Данил Александрович"));
+        onView(withId(R.id.executor_drop_menu_auto_complete_text_view)).perform(click());
+        onView(withId(R.id.date_in_plan_text_input_edit_text)).perform(replaceText("25.04.2023"));
+        onView(withId(R.id.time_in_plan_text_input_edit_text)).perform(replaceText("00:00"));
+        onView(withId(R.id.description_edit_text)).perform(replaceText("1>:{+ Тест Description"));
+        onView(withId(R.id.description_edit_text)).perform(closeSoftKeyboard());
+        onView(withId(R.id.save_button)).perform(click());
+        SystemClock.sleep(2000);
+        ViewInteraction claimsMainWindowCheck = onView(withText("Claims")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void createClaimCommentTest() {
+        this.goToClaimsScreen();
+        onView(withIndex(withId(R.id.claim_bottom_divider_image_view), 1)).perform(click());
+        onView(withId(R.id.add_comment_image_button)).perform(scrollTo()).perform(click());
+        SystemClock.sleep(2000);
+        onView(withId(R.id.comment_text_input_layout)).perform(replaceText("nice comment"), closeSoftKeyboard());
+        SystemClock.sleep(3000);
+        onView(withId(R.id.save_button)).perform(click());
+        SystemClock.sleep(2000);
+        onView(withText("nice comment")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void createClaimEmptyCommentTest() {
+        this.goToClaimsScreen();
+        onView(withIndex(withId(R.id.claim_bottom_divider_image_view), 1)).perform(click());
+        onView(withId(R.id.add_comment_image_button)).perform(scrollTo()).perform(click());
+        SystemClock.sleep(2000);
+        onView(withId(R.id.save_button)).perform(click());
+        onView(withText("The field cannot be empty")).check(matches(isDisplayed()));
+        onView(withText("Cancel")).perform(click());
     }
 }
